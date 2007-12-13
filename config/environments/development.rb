48cbf636347ecef350bc88e8750965d25c5f7d0e
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2007-12-13 15:57:06 +0000

    Support for rails 2.0.1 New prototype and scriptuculas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,14 +9,14 @@ config.cache_classes = false
 config.whiny_nils = true
 
 # Enable the breakpoint server that script/breakpointer connects to
-config.breakpoint_server = true
+# config.breakpoint_server = true
 
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
 config.action_view.cache_template_extensions         = false
 config.action_view.debug_rjs                         = true
-
+config.action_controller.session = { :session_key => "_myapp_session", :secret => "this_is_preparation_to_gmar_tikun600K" }
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 require 'ruby-debug'

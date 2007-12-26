23363f57582490e24771efa043d5c20c12a6e2e0
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2007-12-26 15:14:10 +0000

    check presence according 'required', not hardcoded

--- app/models/rp_number.rb
+++ app/models/rp_number.rb
@@ -1,7 +1,6 @@
 class RpNumber < ResourceProperty	
 
-  validates_presence_of :value
-  validates_numericality_of :value, :only_integer => true
+  validates_numericality_of :value, :only_integer => true, :allow_nil => true
 	
   # validates_numericality_of requires presence of the following function
   alias value_before_type_cast value

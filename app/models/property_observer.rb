d646488128d4746f9c6870ad8e994167e5b5cef9
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2008-03-07 06:04:10 +0000

    bugfix in property_observer.rb

--- app/models/property_observer.rb
+++ app/models/property_observer.rb
@@ -1,6 +1,8 @@
 class PropertyObserver < ActiveRecord::Observer
   def after_update(property)
-    # Update cache and thumbnails upon change of geometry
-    Attachment.update_thumbnails(property) unless old_geometry_string.eql?(geometry_string)
+    if property.respond_to?(:geometry_string) && property.respond_to?(:old_geometry_string)
+      # Update cache and thumbnails upon change of geometry
+      Attachment.update_thumbnails(property) unless old_geometry_string.eql?(geometry_string)
+    end
   end
 end

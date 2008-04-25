cba95aef97af99770ed4bae8ef707400cadb2cac
dimap <dimap@88e0a184-90cd-446e-838e-ca381c575986>
2008-04-25 13:10:22 +0000

    Get action and target

--- app/models/url_migration.rb
+++ app/models/url_migration.rb
@@ -1,2 +1,12 @@
 class UrlMigration < ActiveRecord::Base
+
+  def self.get_action_and_target(source)
+	migration = find :first, :conditions => [ "source = ?", source ]
+	if (migration)	
+		return [migration.action, migration.target]
+	else
+		return ["Not Found", ""]
+	end
+  end
+
 end

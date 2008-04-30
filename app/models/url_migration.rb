39eaf002fc50c0f26d7b4d23320607c5078acf90
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2008-04-30 12:01:20 +0000

    Added documentation for url migration model

--- app/models/url_migration.rb
+++ app/models/url_migration.rb
@@ -1,12 +1,16 @@
+# {:status => '404'} - removed from the system in perpose                                   
+# nil - not found in our table or not active
+# {:status => '301', :target => 'http://www.kab.co.il'} - redirected
 class UrlMigration < ActiveRecord::Base
-
+  
   def self.get_action_and_target(source)
-	migration = find :first, :conditions => [ "source = ?", source ]
-	if (migration)	
-		return [migration.action, migration.target]
-	else
-		return ["Not Found", ""]
-	end
+    migration = find :first, :conditions => [ "source = ?", source ]
+    if (migration)	
+      return [migration.action, migration.target]
+    else
+      return ["Not Found", ""]
+    end
   end
 
 end
+

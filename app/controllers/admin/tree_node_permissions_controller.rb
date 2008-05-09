4b01a35832a8206cfc151eadd60193640b594308
dimam <dimam@88e0a184-90cd-446e-838e-ca381c575986>
2008-05-09 02:44:32 +0000

    new controller and views for configuration of access rights (permissions)

--- app/controllers/admin/tree_node_permissions_controller.rb
+++ app/controllers/admin/tree_node_permissions_controller.rb
@@ -1,12 +1,18 @@
-class Admin::TreeNodeAcRightsController < ApplicationController
+class Admin::TreeNodePermissionsController < ApplicationController
   layout 'admin'
   
   before_filter {|c| c.admin_authorize(['Nodes Access Rights'])}
-
+  before_filter :find_tree_node
+  before_filter :save_refferer_to_session, :only => [ :index ]
+  
   # GET /tree_node_ac_rights
   # GET /tree_node_ac_rights.xml
   def index
-    @tree_node_ac_rights = TreeNodeAcRight.find(:all, :conditions=>"is_automatic=false")
+    if params[:tree_node_id]
+      @tree_node_ac_rights = TreeNodeAcRight.find(:all, :conditions=>["tree_node_id=?", params[:tree_node_id]])
+    else
+      @tree_node_ac_rights = TreeNodeAcRight.find(:all)
+    end
   # @tree_node_ac_rights = TreeNodeAcRight.find_by_sql("select tat.* from tree_node_ac_rights tat
   #   where tat.group_id in (select group_id from groups_users gu where gu.group_id=tat.group_id and user_id =#{AuthenticationModel.current_user})")
 
@@ -20,7 +26,7 @@ class Admin::TreeNodeAcRightsController < ApplicationController
   # GET /tree_node_ac_rights/1.xml
   def show
     @tree_node_ac_rights = TreeNodeAcRight.find(params[:id])
-    @tree_node = @tree_node_ac_rights.get_tree_node
+    #@tree_node = @tree_node_ac_rights.get_tree_node
       
     respond_to do |format|
       format.html # show.html.erb
@@ -52,7 +58,7 @@ class Admin::TreeNodeAcRightsController < ApplicationController
     respond_to do |format|
       if @tree_node_ac_rights.save
         flash[:notice] = 'TreeNodeAcRights was successfully created.'
-        format.html { redirect_to(admin_tree_node_ac_right_path(@tree_node_ac_rights)) }
+        format.html { redirect_to(admin_tree_node_tree_node_permission_path(@tree_node_id, @tree_node_ac_rights)) }
         format.xml  { render :xml => @tree_node_ac_rights, :status => :created, :location => @tree_node_ac_rights }
       else
         format.html { render :action => "new" }
@@ -69,7 +75,7 @@ class Admin::TreeNodeAcRightsController < ApplicationController
     respond_to do |format|
       if @tree_node_ac_rights.update_attributes(params[:tree_node_ac_rights])
         flash[:notice] = 'TreeNodeAcRights was successfully updated.'
-        format.html { redirect_to(admin_tree_node_ac_right_path(@tree_node_ac_rights)) }
+        format.html { redirect_to(admin_tree_node_tree_node_permission_path(@tree_node_id, @tree_node_ac_rights)) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -85,8 +91,21 @@ class Admin::TreeNodeAcRightsController < ApplicationController
     @tree_node_ac_rights.destroy
 
     respond_to do |format|
-      format.html { redirect_to admin_tree_node_ac_rights_path }
+      format.html { redirect_to admin_tree_node_tree_node_permissions_path }
       format.xml  { head :ok }
     end
   end
+  
+  private
+   def find_tree_node
+      @tree_node_id = params[:tree_node_id]
+      if @tree_node_id
+        @tree_node = TreeNode.find(@tree_node_id)
+      
+      end
+      if @tree_node.nil?
+           logger.error("Tree_node #{@tree_node_id} not found.")
+            raise "Tree_node #{@tree_node_id} not found."
+      end
+   end
 end

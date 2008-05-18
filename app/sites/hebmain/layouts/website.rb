23c1768fd523d4eee2acdd5ad99d71e845110fd4
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2008-05-18 21:46:19 +0000

    Home page: tabs are working now

--- app/sites/hebmain/layouts/website.rb
+++ app/sites/hebmain/layouts/website.rb
@@ -18,13 +18,19 @@ class Hebmain::Layouts::Website < WidgetManager::Layout
         title ext_title
         css(get_css_external_url('reset-fonts-grids'))
         css(get_css_external_url('base-min'))
-        css(get_css_external_url('../ext/resources/css/ext-all'))
+#        css(get_css_external_url('../ext/resources/css/ext-all'))
+        css(get_css_external_url('../ext/resources/css/reset'))
+        css(get_css_external_url('../ext/resources/css/core'))
+        css(get_css_external_url('../ext/resources/css/layout'))
+        css(get_css_external_url('../ext/resources/css/panel'))
+        css(get_css_external_url('../ext/resources/css/borders'))
+        css(get_css_external_url('../ext/resources/css/tabs'))
         css(get_css_url('header'))
         css(get_css_url('home_page'))
         css(get_css_url('page_admin'))
         rawtext <<-ExtJS
           <script src="/javascripts/../ext/adapter/ext/ext-base.js" type="text/javascript"></script>
-          <script src="/javascripts/../ext/ext-all-debug.js" type="text/javascript"></script>
+          <script src="/javascripts/../ext/ext-tabs-tree.js" type="text/javascript"></script>
           <script src="/javascripts/ext-helpers.js" type="text/javascript"></script>
         ExtJS
         #javascript(:src => "../javascripts/prototype.js")
@@ -32,6 +38,7 @@ class Hebmain::Layouts::Website < WidgetManager::Layout
         #javascript(:src => "../ext/adapter/prototype/ext-prototype-adapter.js")
         #javascript(:src => "../ext/ext-all-debug.js")
         javascript {
+          rawtext 'Ext.util.CSS.swapStyleSheet("theme","ext/resources/css/xtheme-gray.css");'
           rawtext 'Ext.BLANK_IMAGE_URL="/ext/resources/images/default/s.gif";'
         }
       }
@@ -53,42 +60,75 @@ class Hebmain::Layouts::Website < WidgetManager::Layout
                     }
                     div(:class => 'left-part') {
                       div(:id => 'tabs1', :class => 'radio-TV') {
-                        rawtext ' '
                       }
                       div(:id => 'tv', :class => 'x-hide-display body'){
-                        rawtext 'TV'
+                        rawtext <<-TV
+                        <object classid="clsid:6BF52A52-394A-11D3-B153-00C04F79FAA6"
+style="display:inline;background-color:#000000;" id="player" name="player" type="video/x-ms-wmv"
+  width="200" height="150" standby="Loading Windows Media Player components...">
+<param name="URL" value="http://files.kab.co.il/video/ger_t_rav_bs-matan-tora_2008-05-18_shiur_bb.wmv" />
+<param name="AutoStart" value="1" /><param name="AutoPlay" value="1" />
+<param name="volume" value="50" /><param name="uiMode" value="none" />
+<param name="animationAtStart" value="1" /><param name="showDisplay" value="1" />
+<param name="transparentAtStart" value="0" />
+<param name="bgcolor" value="#000000" />
+<param name="balance" value="0" />
+  <embed id="player" name="player" style="display:inline;background-color:#000000;"
+src="http://files.kab.co.il/video/ger_t_rav_bs-matan-tora_2008-05-18_shiur_bb.wmv"
+ type="video/x-ms-wmv"
+pluginspage="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112"
+ autostart="true" uimode="none" width="200" height="150" />
+</object>
+                        TV
+                        div(:class => 'clear'){
+                          a _('Watch in full window'), :href => ''
+                        }
                       }
                       div(:id => 'radio', :class => 'x-hide-display body'){
                         rawtext 'RADIO'
                       }
-                      javascript() {
+                      javascript {
                         # Start onReady
                         rawtext <<-EXT_ONREADY
                           Ext.onReady(function(){
                             tabs();
                           });
-                        EXT_ONREADY
-
-                        rawtext <<-EXT_TABS
                           tabs = function(){
                             var mytab = new Ext.TabPanel({
                               renderTo: 'tabs1',
                               width:222,
-                              activeTab:1,
+                              activeTab:0,
                               frame:true,
-                              border:false,
                               resizeTabs:true,
-                              plain:true,
-                              defaults:{
-                                autoHeight: true
-                              },
                               items:[
-                                  {contentEl:'tv', title: 'טלויזיה' },
-                                  {contentEl:'radio', title: 'רדיו'}
+                                  {contentEl:'radio', title: 'רדיו' },
+                                  {contentEl:'tv', title: 'טלויזיה' }
                               ]
                             });
+                            mytab.on('tabchange', function(panel, tab){
+                              if (tab.contentEl == "tv"){
+                                startPlayer();
+                              } else {
+                                pausePlayer();
+                              }
+                            });
+function startPlayer()
+{
+var player = document.getElementById("player");
+ if (player && player.controls && player.controls.isAvailable('Play')){
+ player.controls.play();
+}
+}
+
+function pausePlayer()
+{
+var player = document.getElementById("player");
+ if (player && player.controls && player.controls.isAvailable('Pause')) {
+ player.controls.pause();
+}
+}
                           }
-                        EXT_TABS
+                        EXT_ONREADY
                       }
                       div(:class => 'blog-rav'){
                         h3 {

42e4f2f49508c0a20396dff50f3d3d4f61e54d62
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2008-05-25 14:52:51 +0000

    new header

--- app/sites/hebmain/widgets/header.rb
+++ app/sites/hebmain/widgets/header.rb
@@ -1,12 +1,12 @@
 class Hebmain::Widgets::Header < WidgetManager::Base
   
-  def render_left
+  def render_top_links
     div(:class => 'search') do
       img(:src => img_path('search.gif'), :alt => 'Search')
       input(:name => 'search')
     end
     ul(:class => 'links') do      
-      external_sections.each do |e|
+      top_links.each do |e|
         li do
           w_class('link').new(:tree_node => e, :view_mode => 'with_image').render_to(self)
         end
@@ -14,7 +14,7 @@ class Hebmain::Widgets::Header < WidgetManager::Base
     end
   end
   
-  def render_right
+  def render_logo
     div(:class => 'logo') do
       h1 'קבלה לעם'
       img(:src => img_path('logo.gif'), :alt => 'Title')
@@ -23,11 +23,21 @@ class Hebmain::Widgets::Header < WidgetManager::Base
   
   private
   
-  def external_sections
+  def top_links
     TreeNode.get_subtree(
       :parent => presenter.website_node.id, 
       :resource_type_hrids => ['link'], 
       :depth => 1,
+      # :placeholder => 'top_links',
+      :has_url => false
+    )               
+  end    
+  def bottom_links
+    TreeNode.get_subtree(
+      :parent => presenter.website_node.id, 
+      :resource_type_hrids => ['link'], 
+      :depth => 1,
+      # :placeholder => 'bottom_links',
       :has_url => false
     )               
   end    

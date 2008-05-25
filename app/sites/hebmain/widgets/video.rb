b99856033ca4f052a78bb222be286e2bfe5c860f
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2008-05-25 12:26:16 +0000

    video css general css

--- app/sites/hebmain/widgets/video.rb
+++ app/sites/hebmain/widgets/video.rb
@@ -18,98 +18,48 @@ class Hebmain::Widgets::Video < WidgetManager::Base
         p { rawtext description if description }
         div(:id => "flashplayer-#{tree_node.object_id}")
         javascript {
-        rawtext <<-Player
-        flashembed("flashplayer-#{tree_node.object_id}", {src:'/flowplayer/FlowPlayerLight.swf', width:447,  
-        height:340}, {config: {
+          rawtext <<-Player
+          flashembed("flashplayer-#{tree_node.object_id}", {src:'/flowplayer/FlowPlayerLight.swf', width:504,  
+          height:378}, {config: {
 
-            autoPlay: false,
-            videoFile: '#{get_flash_url}',
-            initialScale: 'scale', 
-            useNativeFullScreen: true,
-            showVolumeSlider: false,
-            controlsOverVideo: 'ease',
-            controlBarBackgroundColor: -1,
-            controlBarGloss: 'low',
-            menuItems: [ 0, 0, 0, 0, 0, 1, 1 ]
-          }} 
-        );
-        Player
-        # rawtext <<-Player
-        #   var player_#{tree_node.object_id};
-        #   writeSWF("flashplayer/info.xml", "flashplayer/skin.swf", player_#{tree_node.object_id}, "flashplayer-#{tree_node.object_id}");
-        # Player
+              autoPlay: false,
+              loop: false,
+              videoFile: '#{get_flash_url}',
+              initialScale: 'scale', 
+              useNativeFullScreen: true,
+              showVolumeSlider: false,
+              controlsOverVideo: 'ease',
+              controlBarBackgroundColor: -1,
+              controlBarGloss: 'low',
+              menuItems: [ 0, 0, 0, 0, 0, 1, 1 ]
+            }} 
+          );
+          Player
         }
         # image = get_preview_image
         # img(:src => image, :alt => 'player') if image
         
-#        rawtext <<-code
-#          <!-- include flashembed -->
-#          <script type="text/javascript" src="/flash/flashembed.min.js"></script>
-#
-#          <script>
-#          /*
-#           * window.onload event occurs after all HTML elements have been loaded
-#           * this is a good place to setup your Flash elements
-#           */
-#          window.onload = function() {  
-#
-#              /*
-#               * flashembed places Flowplayer into HTML element 
-#               * whose id="example" (see below this script tag)
-#               */
-#              flashembed("example", 
-#
-#                /*
-#                 * second argument supplies standard Flash parameters.
-#                 * basically these are all you want to modify
-#                 * but you can see full list here
-#                 */
-#                {
-#                   src:'/flash/FlowPlayerLight.swf',
-#                   width:400,  
-#                   height:300,
-#                   bgcolor:'#ffffff'
-#                },
-#
-#                /*
-#                 * third argument is Flowplayer specific configuration
-#                 * full list of options is given here
-#                 */
-#                {config: {   
-#                   videoFile: '/flash/example.flv',
-#                   initialScale: 'scale', 
-#                   useNativeFullScreen: true
-#
-#                   // supply more options here by separating them with commas
-#                }} 
-#             );
-#          }
-#          </script>
-#
-#          <!-- this DIV is where your Flowplayer will be placed. it can be anywhere -->
-#          <div id="example"></div>
-#        code
       }
   
       div(:class => 'embed'){
-        span(:class => 'text') {
-          text '  Embed'
-        }
-        input(:id => 'addr', 
-          :value =>'<object width="425" height=...',
-          :readonly => 'readonly',
-          :onclick => "javascript:document.getElementById('addr').focus();document.getElementById('addr').select()")
-        span(:class => 'services'){
-          img(:src => "/images/hebmain/player/pipe.gif", :alt => "")
-          a(:href => get_url, :title => 'arrow') {
-            img(:src => '/images/hebmain/player/arrow.gif', :alt => 'arrow')          
-          }
-          img(:src => '/images/hebmain/player/pipe.gif', :alt => '')
-
-          a(:href => '#', :title => 'email', :alt => 'email') {
-            img(:src => '/images/hebmain/player/email.gif', :alt => 'email')          
-          }
-        }
+        # span(:class => 'text') {
+        #   text '  Embed'
+        # }
+        # input(:id => 'addr', 
+        #   :value =>'<object width="425" height=...',
+        #   :readonly => 'readonly',
+        #   :onclick => "javascript:document.getElementById('addr').focus();document.getElementById('addr').select()")
+        # span(:class => 'services'){
+        #   img(:src => "/images/hebmain/player/pipe.gif", :alt => "")
+        #   a(:href => get_url, :title => 'arrow') {
+        #     img(:src => '/images/hebmain/player/arrow.gif', :alt => 'arrow')          
+        #   }
+        #   img(:src => '/images/hebmain/player/pipe.gif', :alt => '')
+        # 
+        #   a(:href => '#', :title => 'email', :alt => 'email') {
+        #     img(:src => '/images/hebmain/player/email.gif', :alt => 'email')          
+        #   }
+        # }
       }
   end
 end

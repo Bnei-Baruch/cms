b6bc099b8c9b49ab13f5c32545608f900dcdc85f
dimap <dimap@88e0a184-90cd-446e-838e-ca381c575986>
2008-06-06 09:17:47 +0000

    Box hide border

--- app/sites/hebmain/widgets/box.rb
+++ app/sites/hebmain/widgets/box.rb
@@ -1,15 +1,21 @@
 class Hebmain::Widgets::Box < WidgetManager::Base
   
   def render_related_items
+    hide_border = get_hide_border
+    if !hide_border.nil? && hide_border
+      no_border = '-no-border'
+    else
+      no_border = ''
+    end
     div(:class => 'box') {
-      div(:class => 'box-top'){rawtext('&nbsp;')}
-      div(:class => 'box-mid'){
+      div(:class => 'box-top' + no_border){rawtext('&nbsp;')}
+      div(:class => 'box-mid' + no_border){
         h4 get_title unless get_title.empty?
         rawtext get_body
         br
         w_class('cms_actions').new(:tree_node => tree_node, :options => {:buttons => %W{ delete_button  edit_button }}).render_to(doc)
       }
-      div(:class => 'box-bot'){rawtext('&nbsp;')}
+      div(:class => 'box-bot' + no_border){rawtext('&nbsp;')}
     }
   end
   

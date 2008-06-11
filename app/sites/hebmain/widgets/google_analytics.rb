fe524e0a14d23a306bf7386ae0d454e8bb009f20
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2008-06-11 07:50:13 +0000

    changed to the old google analytics code to check if unicode URL's will work

--- app/sites/hebmain/widgets/google_analytics.rb
+++ app/sites/hebmain/widgets/google_analytics.rb
@@ -3,15 +3,24 @@ class Hebmain::Widgets::GoogleAnalytics < WidgetManager::Base
   def render_full
         rawtext <<-Google
 
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
 <script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-548326-62");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
-        Google
+_uacct = "UA-548326-62";
+urchinTracker();
+</script>        
+                  Google
+#         rawtext <<-Google
+# 
+# <script type="text/javascript">
+# var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+# document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+# </script>
+# <script type="text/javascript">
+# var pageTracker = _gat._getTracker("UA-548326-62");
+# pageTracker._initData();
+# pageTracker._trackPageview();
+# </script>
+#         Google
    end
 end

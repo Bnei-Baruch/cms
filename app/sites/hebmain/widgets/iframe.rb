46cf2cc99c9175a9566d62293bae9fa32191d672
dimap <dimap@88e0a184-90cd-446e-838e-ca381c575986>
2008-08-15 08:56:10 +0000

    Iframe widget

---
+++ app/sites/hebmain/widgets/iframe.rb
@@ -0,0 +1,13 @@
+class Hebmain::Widgets::Iframe < WidgetManager::Base
+  
+  def render_full
+    w_class('cms_actions').new(:tree_node => tree_node, :options => {:buttons => %W{ delete_button  edit_button }}).render_to(doc)
+    url = get_url
+    unless url.empty?
+      title = get_title || ''
+      height = get_height || 500
+      iframe(:src => url, :title => title, :width => '100%', :height => height.to_s)
+    end
+  end
+  
+end

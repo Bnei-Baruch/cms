42676c8a4080feea9e2b736f2d1e49d0044e9e03
dimap <dimap@88e0a184-90cd-446e-838e-ca381c575986>
2008-08-22 10:33:45 +0000

    Iframe

--- app/sites/hebmain/widgets/iframe.rb
+++ app/sites/hebmain/widgets/iframe.rb
@@ -6,7 +6,7 @@ class Hebmain::Widgets::Iframe < WidgetManager::Base
     unless url.empty?
       title = get_title || ''
       height = get_height || 500
-      iframe(:src => url, :title => title, :width => '100%', :height => height.to_s)
+      iframe(:class => 'iframe_no_border', :src => url, :title => title, :width => '100%', :height => height.to_s)
     end
   end
   

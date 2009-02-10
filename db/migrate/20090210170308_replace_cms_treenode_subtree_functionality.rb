0599e8cc116d9a82991110fc16a832f3cbdbfe38
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2009-02-10 17:41:44 +0000

    tree_node_subtree replacement

--- db/migrate/20090210170308_replace_cms_treenode_subtree_functionality.rb
+++ db/migrate/20090210170308_replace_cms_treenode_subtree_functionality.rb
@@ -432,5 +432,123 @@ class ReplaceCmsTreenodeSubtreeFunctionality < ActiveRecord::Migration
   end
 
   def self.down
+    sql = <<-my_code
+        SET client_encoding = 'UTF8';
+        SET standard_conforming_strings = off;
+        SET check_function_bodies = false;
+        SET client_min_messages = warning;
+        SET escape_string_warning = off;
+
+        -- SET search_path = cms_test, pg_catalog;
+        SET default_tablespace = '';
+        SET default_with_oids = false;
+
+        DELETE TRIGGER cms_cache_properties_trigger;
+        DELETE TRIGGER cms_cache_resource_properties_trigger;
+        DELETE TRIGGER cms_cache_resources_trigger;
+
+      CREATE OR REPLACE FUNCTION cms_treenode_subtree(
+      	p_tn_id 	integer, 			-- required
+              p_user_id       integer, 			-- required
+      	p_res_hrids	varchar[],  			-- optional
+      	p_is_main 	boolean,  			-- optional
+      	p_has_url 	boolean,  			-- optional
+      	p_depth 	integer,  			-- optional
+      	p_where 	character varying,  		-- optional
+      	p_page_num 	integer,  			-- optional
+      	p_page_size 	integer,  			-- optional
+      	p_return_parent boolean,  			-- optional
+              p_placeholders  character varying[],  		-- optional
+              p_statuses      character varying[])  		-- optional
+      	RETURNS SETOF tree_nodes AS $BODY$
+      DECLARE
+        tn_rec       tree_nodes%rowtype;
+        rec          record;
+        l_page_size  integer   := p_page_size;
+        l_item       integer   := 0;
+        l_item_first integer   := 0;
+        l_item_last  integer   := 2147483647;
+        l_skip_level integer   := 0;
+        l_statuses   varchar[] := COALESCE(p_statuses, ARRAY['PUBLISHED']);
+      BEGIN
+
+        -- Determine page size
+        if l_page_size is null or l_page_size <= 0 then
+          l_page_size := 25;
+        end if;
+
+        -- According to page size - lets set first and last numbers of records
+        if p_page_num is not null then
+          if p_page_num >= 0 then
+            l_item_first := p_page_num * l_page_size;
+            l_item_last := l_item_first + l_page_size;
+          end if;
+        end if;
+
+        -- Main loop
+        FOR rec IN SELECT
+            t.id, t.level, b.status, COALESCE(c.max_ac_type, 0) as max_ac_type
+          FROM
+            connectby('tree_nodes', 'id', 'parent_id', 'position',  cast(p_tn_id AS text), COALESCE(p_depth, 0)) AS t(id int, parent_id int, level integer, position int)
+            join tree_nodes                                a on (t.id = a.id)
+            join resources                                 b on (a.resource_id = b.id)
+            left join cms_all_treenode_max_user_permission c on (c.tree_node_id = t.id and p_user_id = c.user_id)
+          WHERE
+            (p_has_url is null      or a.has_url = p_has_url) and
+            (p_is_main is null      or a.is_main = p_is_main) and
+            (p_res_hrids is null    or b.resource_type_id IN (select id from resource_types where hrid = ANY (p_res_hrids))) and
+            (p_placeholders is null or a.placeholder = ANY (p_placeholders)) and
+            (p_where is null        or a.resource_id IN (select * from cms_res_properties_where_pipe(p_where)))
+          ORDER BY
+            t.position
+        LOOP
+
+          -- Its parent and not to return it? continue
+          continue when not COALESCE(p_return_parent, false) and rec.id = p_tn_id;
+
+          -- Current node is below the skipped one? continue
+          continue when l_skip_level > 0 and rec.level > l_skip_level;
+
+          -- User does not have permission on this node? skip level & continue
+          -- if the resource of that node is PUBLISHED or ARCHIVED and the group has ac_type >= 1 it is returned
+          -- if the resource of that node is DRAFT and the group has ac_type >= 2 it is returned
+          -- if the resource of that node is DELETED and the group has ac_type >= 3 it is returned
+          if rec.max_ac_type <= 0 or (rec.status = 'DRAFT' and rec.max_ac_type < 2) or (rec.status = 'DELETED' and rec.max_ac_type < 3) then
+            l_skip_level := rec.level;
+            continue;
+          end if;
+
+          -- Define skip level on resource STATUS if appropriate
+          if rec.level > 0 then
+            if rec.status = ANY (l_statuses) then
+              l_skip_level := 0;
+            else
+              l_skip_level := rec.level;
+              continue;
+            end if;
+          end if;
+
+          -- increase the actual item number
+          l_item := l_item + 1;
+
+          -- Paging stuff: Already above page? exit or Still not reached first item on page? continue looping
+          exit when l_item >= l_item_last;
+          continue when l_item < l_item_first;
+
+          -- At last! Return next record!
+          select * into tn_rec from tree_nodes where id = rec.id;
+          tn_rec.max_user_permission := rec.max_ac_type;
+          RETURN NEXT tn_rec;
+
+        END LOOP;
+
+        RETURN;
+
+      END
+      $BODY$
+        LANGUAGE 'plpgsql' VOLATILE;
+
+    my_code
+    execute sql
   end
 end

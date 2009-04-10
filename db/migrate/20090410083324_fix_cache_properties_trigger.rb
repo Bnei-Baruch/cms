aef721be347d86fbce440a69ea95773723a7c3cc
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2009-04-10 09:04:47 +0000

    removed unneeded migration

--- db/migrate/20090410083324_fix_cache_properties_trigger.rb
+++ db/migrate/20090410083324_fix_cache_properties_trigger.rb
@@ -1,48 +1,5 @@
 class FixCachePropertiesTrigger < ActiveRecord::Migration
   def self.up
-    sql = <<-my_code
-    CREATE OR REPLACE FUNCTION cms_cache_properties_alter()
-      RETURNS trigger AS
-    $BODY$
-              DECLARE
-                l_fname  varchar;
-                l_ftype  varchar;
-                l_etype  varchar;
-                l_exists integer;
-              BEGIN
-
-            if TG_OP = 'INSERT' then
-              SELECT cms_cache_property_field_name(NEW.hrid, NEW.field_type), cms_cache_property_field_type(NEW.field_type) into l_fname, l_ftype;
-              select count(*) into l_exists from cms_util_describe where "Table" = 'cms_cache_resource_properties' and "Field" = l_fname;
-
-              if l_exists > 0 then
-                select "Type" into l_etype from cms_util_describe where "Table" = 'cms_cache_resource_properties' and "Field" = l_fname;
-                if l_etype <> l_ftype then
-                  RAISE EXCEPTION 'Existing % field type % does not match new type % in cache table', l_fname, l_etype, l_ftype;
-                end if;
-              else
-                execute 'ALTER TABLE "cms_cache_resource_properties" ADD COLUMN ' || l_fname || ' ' || l_ftype;
-              end if;
-            elsif TG_OP = 'DELETE' then
-              SELECT cms_cache_property_field_name(OLD.hrid, OLD.field_type), cms_cache_property_field_type(OLD.field_type) into l_fname, l_ftype;
-              SELECT count(*) into l_exists from cms_cache_properties_fields where field_name = l_fname and field_type = l_ftype;
-
-              if l_exists = 0 then
-                execute 'ALTER TABLE "cms_cache_resource_properties" DROP COLUMN ' || l_fname;
-              end if;
-            elsif TG_OP = 'UPDATE' and OLD.hrid <> NEW.hrid then
-              RAISE EXCEPTION 'Field properties.hrid can not be updated because of the cache table';
-            end if;
-
-                  RETURN NEW;
-
-              END;
-          $BODY$
-      LANGUAGE 'plpgsql' VOLATILE;
- 
-     my_code
-    execute sql
-
   end
 
   def self.down

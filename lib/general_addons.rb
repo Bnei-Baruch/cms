ce5a4add3a5b6e2065fec0399b1a4fece65945ba
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2009-04-17 05:38:49 +0000

    translation -- be a bit more user-friendly in case of lack of translation

--- lib/general_addons.rb
+++ lib/general_addons.rb
@@ -2,13 +2,14 @@ module I18n
   class << self
     def default_translation(exception, locale, key, options)
       if MissingTranslationData === exception
-        case key.class
-        when String
+        if key.is_a?(String)
           return key
-        when Symbol
-          return key.humanize
+        elsif key.is_a?(Symbol)
+          key.to_s =~ /.*\.([^.]+)/
+          return $1.blank? ? key.to_s : $1.humanize
+        else
+          return key.to_s.humanize
         end
-        return key.to_s.humanize
         #raise exception
       end
     end  
@@ -23,30 +24,30 @@ class Object
 end
 
 class NilClass
-def empty?
-  true
-end
+  def empty?
+    true
+  end
 end
 
 module ActiveRecord
-class Migration
+  class Migration
     
-  class << self
-    # Login before migration in order to update DB
-    def migration_login
-      msession = Hash.new
-      username = $config_manager.appl_settings[:cron_manager_login_user][:username]
-      password = $config_manager.appl_settings[:cron_manager_login_user][:password]
-      user = User.authenticate(username, password)
-      if user
-        msession[:user_id] = user.id
-        msession[:current_user_is_admin] = user.groups.find(:all, :conditions => {:groupname=>'Administrators'}).length
-      else
-        raise "Access denied for Rss Reader user."
+    class << self
+      # Login before migration in order to update DB
+      def migration_login
+        msession = Hash.new
+        username = $config_manager.appl_settings[:cron_manager_login_user][:username]
+        password = $config_manager.appl_settings[:cron_manager_login_user][:password]
+        user = User.authenticate(username, password)
+        if user
+          msession[:user_id] = user.id
+          msession[:current_user_is_admin] = user.groups.find(:all, :conditions => {:groupname=>'Administrators'}).length
+        else
+          raise "Access denied for Rss Reader user."
+        end
+        Thread.current[:session] = msession
       end
-      Thread.current[:session] = msession
-    end
       
+    end
   end
-end
 end

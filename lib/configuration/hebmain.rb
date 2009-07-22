78bbea13bd3ff146e877e1042f5dff1590e00985
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2009-07-22 16:07:25 +0000

    CMS-58 Google Analytics

--- lib/configuration/hebmain.rb
+++ lib/configuration/hebmain.rb
@@ -1,21 +1,21 @@
 # General configuration - if no other configuration module found this is the last override file
 module	Configuration::Hebmain
   SETTINGS = {
-# define the site view directory under app/views/sites/
+    # define the site view directory under app/views/sites/
     :site_name => 'hebmain', 
- # define the group view directory under app/views/sites/ - 
- # this is an override after the content is not found in 'site_dir'
+    # define the group view directory under app/views/sites/ -
+    # this is an override after the content is not found in 'site_dir'
     :group_name => 'mainsites',
- # define the interface language (for the frontend). This is powered by a multilingual plugin
+    # define the interface language (for the frontend). This is powered by a multilingual plugin
     :language => 'hebrew',
     :short_language => 'he',
     :site_direction => 'rtl',
-# Map specific resource to custom layuot 
-# <resource_type> => <layout_name>
+    # Map specific resource to custom layuot
+    # <resource_type> => <layout_name>
     :layout_map => { 
       'search' => 'content_page'
-      },
- # define view modes per parent widget and placeholder.
+    },
+    # define view modes per parent widget and placeholder.
     :view_modes => {
       # parent widget
       :website => {
@@ -41,11 +41,13 @@ module	Configuration::Hebmain
         }
       }
     },
-  # define widgets that should not have bottom border  
+    # define widgets that should not have bottom border
     :disable_bottom_border => ['title'],
-    # width of the result of the Google Search
+    # Google Search
     :google_result_width => 520,
     :search_id => '011301558357120452512:ulicov2mspu',
     :forid => 'FORID:11',
+    # Google analytics
+    :ua => 'UA-548326-62',
   }
 end

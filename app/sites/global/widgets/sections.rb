715dcd38509980577a538e24f0130ddae062978b
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2009-09-07 16:51:23 +0000

    CMS-222 Order of items in upper menu differs from the static/dynamic ones

--- app/sites/global/widgets/sections.rb
+++ app/sites/global/widgets/sections.rb
@@ -1,25 +1,31 @@
 class Global::Widgets::Sections < WidgetManager::Base
-  
+
   def render_full
     ul(:class => 'sf-menu'){
+      # The first item is Homepage
       li{
         a({:href => presenter.home}.merge!(gg_analytics_tracking('homepage'))){
           img(:src => img_path('home.gif'), :alt => 'Homepage')
         }
       }
+      # Only sections will be present in tree
+      # Each item in tree is section and has array of :children
       tree = []
+      idx = -1
       main_sections.inject(0){ |parent_id, section|
-        if section.parent_id == parent_id # i.e. this is child
-          tree[parent_id][:children] << section
+        if section.parent_id == parent_id
+          ## this is child of the previousely found section
+          tree[idx][:children] << section
         else
-          id = section.id
-          tree[id] = section
-          tree[id][:children] = []
-          parent_id = id
+          # Section was found
+          idx += 1
+          tree[idx] = section
+          tree[idx][:children] = []
+          parent_id = section.id
         end
         parent_id
       }
-      tree.compact.each{ |section|
+      tree.each{ |section|
         li{
           a({:href => get_page_url(section)}.merge!(gg_analytics_tracking('top-level', section.resource.name))) {
             span {rawtext section.resource.name}

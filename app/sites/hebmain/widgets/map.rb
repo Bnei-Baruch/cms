80ea2cb2c4747c4550350f3ee6182403ecd21898
yohan <yohan@88e0a184-90cd-446e-838e-ca381c575986>
2009-10-01 14:07:07 +0000

    table in map widgets.css

--- app/sites/hebmain/widgets/map.rb
+++ app/sites/hebmain/widgets/map.rb
@@ -12,12 +12,56 @@ class Hebmain::Widgets::Map < Mainsites::Widgets::Sitemap
       br
       div(:class => 'sitemap'){
         div(:class => 'sitemap-inner'){
-          
+          table{
+            presenter.main_sections.each_with_index {|section,index|
+              if(index%3==0)
+                rawtext "<tr>"
+              end
+              sub_sections = get_sub_section(section) || []
+                td{
+                  ul(:class => 'box index'+index.to_s){
+                    li {
+                      a section.resource.name, :href => get_page_url(section), :class => 'title'
+                      ul{
+                        sub_sections.each {|section|
+                          li(:class => 'list'){
+                            a(:href => get_page_url(section)){text section.resource.name}
+                          }
+                        }
+                      }
+                    }
+                  }
+                } unless sub_sections.empty?
+              if(index%3==2)
+                rawtext "</tr>"
+              end
+            }
+            div(:class => 'clear')
+          }
+        }
+      }
+    }
+  end
+  
+  def render_with_div
+    w_class('cms_actions').new( :tree_node => tree_node,
+      :options => {:buttons => %W{ edit_button delete_button },
+        :resource_types => %W{ map },
+        :new_text => 'צור יחידת תוכן חדשה',
+        :has_url => false
+      }).render_to(self)
+
+    div(:class => "pagemap"){
+      h3 {text get_title}
+      br
+      div(:class => 'sitemap'){
+        div(:class => 'sitemap-inner'){
+
           presenter.main_sections.each_with_index {|section,index|
-            
+
             sub_sections = get_sub_section(section) || []
 
-             ul(:class => 'box index'+index.to_s){
+            ul(:class => 'box index'+index.to_s){
               li {
                 a section.resource.name, :href => get_page_url(section), :class => 'title'
                 ul{

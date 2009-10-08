3042d8e6fc672e384615bb6a3fe03b1b3e8251d4
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2009-10-08 16:25:20 +0000

    CMS-294 Search page doesn't work - fix

---
+++ db/migrate/20091008162137_add_hide_breadcrumbs_to_search.rb
@@ -0,0 +1,31 @@
+class AddHideBreadcrumbsToSearch < ActiveRecord::Migration
+  def self.up
+    migration_login
+
+    resource_type = ResourceType.find(:first, :conditions => {:hrid => 'search'})
+    raise 'Failed to find search resource type' unless resource_type
+
+    property = Property.new(
+      :name => 'Hide Breadcrumbs',
+      :field_type => 'Boolean',
+      :hrid => 'hide_breadcrumbs',
+      :resource_type_id => resource_type.id,
+      :is_required => false)
+    raise 'Failed to create property \'Hide Breadcrumbs\'' unless property
+    property.save!
+
+  end
+
+  def self.down
+    migration_login
+    
+    resource_type = ResourceType.find(:first, :conditions => {:hrid => 'search'})
+    return unless resource_type
+
+    property = Property.find(:first, :conditions => {:hrid => 'hide_breadcrumbs', :resource_type_id => resource_type.id})
+    return unless property
+    resource_properties = ResourceProperty.find(:all, :conditions => {:property_id => property.id})
+    resource_properties.each{ |rp| rp.delete }
+    property.delete
+  end
+end

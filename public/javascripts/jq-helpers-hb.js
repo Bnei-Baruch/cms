5d82bea9cbaa860bd9da5d7b4c2036fe4292b4bb
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2009-11-15 18:19:08 +0000

    New flowplayer

--- public/javascripts/jq-helpers-hb.js
+++ public/javascripts/jq-helpers-hb.js
@@ -600,7 +600,6 @@ $(function() {
   
     var $links = $player.find("a").not(".more").click(function(event){
         var $this = $(this);
-        $(".play-list-button").hide();
         event.preventDefault();
         playerConfig_innerpage.videoFile = $this.attr("href");
         if (flowplayer_innerpage == null) {
@@ -633,6 +632,7 @@ $(function() {
         // Find out and replace title
         var hdr = $($this.parent().children('.h1-play')[0]).text();
         $this.parents('.inner-player').find('.play-title').text(hdr);
+        $(".play-list-button").hide();
         return false;
     });
     $("#flashplayer-"+id+" img").click(function(){

53bd02e6393466adeaf10762cc4a6c4b3dab0748
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2010-03-14 12:58:34 +0000

    HEB-165 1. Add option to open the banner in the same page (content editor should select).

---
+++ db/migrate/20100314125314_banner_link_may_be_external.rb
@@ -0,0 +1,36 @@
+class BannerLinkMayBeExternal < ActiveRecord::Migration
+  def self.up
+    migration_login
+
+    resource_type = ResourceType.find(:first, :conditions => {:hrid => 'banner'})
+    raise 'Failed to find banner resource type' unless resource_type
+
+    rid = resource_type.id
+    property = Property.new(
+      :name => 'Internal Link',
+      :field_type => 'Boolean',
+      :hrid => 'internal_link',
+      :resource_type_id => rid,
+      :position => generate_position(rid),
+      :is_required => false)
+    raise 'Failed to create property \'Internal Link\'' unless property
+    property.save!
+
+    update_resource_properties(rid)
+  end
+
+  def self.down
+    migration_login
+
+    resource_type = ResourceType.find(:first, :conditions => {:hrid => 'banner'})
+    return unless resource_type
+
+    property = Property.find(:first, :conditions => {:hrid => 'internal_link', :resource_type_id => resource_type.id})
+    if property
+      resource_properties = ResourceProperty.find(:all, :conditions => {:property_id => property.id})
+      resource_properties.each{ |rp| rp.delete }
+      property.delete
+    end
+
+  end
+end

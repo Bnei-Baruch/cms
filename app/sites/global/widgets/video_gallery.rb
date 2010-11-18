55495d38543daa1d4590bf013a5983d9d6c43d18
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2010-11-18 13:46:41 +0000

    new version of flowplayer-3.2.5, commercial license

--- app/sites/global/widgets/video_gallery.rb
+++ app/sites/global/widgets/video_gallery.rb
@@ -26,7 +26,7 @@ class Global::Widgets::VideoGallery < WidgetManager::Base
         rawtext <<-Embedjs
           $(document).ready(function() {
                flowplayer('flashplayer-#{id}',{src: '/flowplayer/flowplayer.commercial-3.2.5.swf', wmode: 'transparent'},{
-                  key:'#\@932a7f91ab5747d7f90',
+                  key:'#\@f0b2be6a10fb2019139',
                   clip:{
                     scaling: 'scale',
                     autoPlay: false,

922d57259a4f25efd1710f4be99bd2eb9b25de6b
gshilin <gshilin@gmail.com>
2012-07-31 06:04:37 +0300

    support for banners on ligdol.tv

--- app/sites/ligdoltv/layouts/website.rb
+++ app/sites/ligdoltv/layouts/website.rb
@@ -12,15 +12,15 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
     @header_logo = w_class('header').new(:view_mode => 'logo')
     @header_copyright = w_class('header').new(:view_mode => 'copyright')
     @breadcrumbs = w_class('breadcrumbs').new()
-    @titles = w_class('breadcrumbs').new(:view_mode => 'titles')  
+    @titles = w_class('breadcrumbs').new(:view_mode => 'titles')
     @dynamic_tree = w_class('tree').new(:view_mode => 'dynamic', :display_hidden => true)
     @google_analytics = w_class('google_analytics').new
-    @newsletter = w_class('newsletter').new(:view_mode => 'sidebar')  if @site_config[:newsletters][:use]
+    @newsletter = w_class('newsletter').new(:view_mode => 'sidebar') if @site_config[:newsletters][:use]
     @languages = w_class('language_menu').new
   end
 
   def render
-    
+
     rawtext '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
     html("xmlns" => "http://www.w3.org/1999/xhtml", "xml:lang" => "en", "lang" => "en", :id => "#{@site_config[:site_name]}") {
       head {
@@ -31,19 +31,19 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
         meta(:name => 'description', :content => ext_meta_description)
 
         javascript_include_tag 'flowplayer-3.2.4.min.js', 'flashembed.min.js'
-        javascript_include_tag 'jquery', 
-        'ui/ui.core.min.js',
-        'ui/jquery.color.js',
-        'jquery.browser.js', 'jq-helpers-hb',
-        :cache => "cache_website-#{@presenter.website_hrid}"
+        javascript_include_tag 'jquery',
+                               'ui/ui.core.min.js',
+                               'ui/jquery.color.js',
+                               'jquery.browser.js', 'jq-helpers-hb',
+                               :cache => "cache_website-#{@presenter.website_hrid}"
 
         stylesheet_link_tag 'reset-fonts-grids',
-        'base-min',
-        'hebmain/common',
-        'hebmain/header',
-        'hebmain/home_page',
-        'hebmain/widgets',
-        :cache => "cache_website-#{@presenter.website_hrid}"
+                            'base-min',
+                            'hebmain/common',
+                            'hebmain/header',
+                            'hebmain/home_page',
+                            'hebmain/widgets',
+                            :cache => "cache_website-#{@presenter.website_hrid}"
 
         site_name = @site_config[:site_name]
         stylesheet_link_tag "#{site_name}/#{site_name}"
@@ -53,8 +53,8 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
         if  presenter.node.can_edit? || perm >= 2 # STUPID, but there are no constatns yet...!!!
           stylesheet_link_tag '../ext/resources/css/ext-all', 'hebmain/page_admin'
           javascript_include_tag '../ext/adapter/ext/ext-base', '../ext/ext-all', 'ext-helpers',
-          'ui/ui.sortable.min.js', 'ui/ui.draggable.min.js', 'ui/ui.droppable.min.js',
-          :cache => "cache_website_admin-#{@presenter.website_hrid}"
+                                 'ui/ui.sortable.min.js', 'ui/ui.draggable.min.js', 'ui/ui.droppable.min.js',
+                                 :cache => "cache_website_admin-#{@presenter.website_hrid}"
           javascript {
             rawtext 'Ext.BLANK_IMAGE_URL="/ext/resources/images/default/s.gif";'
           }
@@ -118,42 +118,42 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
                   end
                   div(:class => 'menu') {
                     w_class('sections').new.render_to(self)
-                  }    
+                  }
                   div(:class => 'yui-u first') {
                     div(:class => 'left-part') {
                       # streamer (hardcoded)
                       div(:class => 'h1') {
                         text ''
-                        div(:class =>'h1-right')
-                        div(:class =>'h1-left')
+                        div(:class => 'h1-right')
+                        div(:class => 'h1-left')
                       }
 
                       #render_hardcoded_streamer('http://switch3.castup.net/cunet/gm.asp?ClipMediaID=160788', 199, 214)
                       #render_hardcoded_streamer('mms://vod.kab.tv/ligdoltv', 199, 214)
 
                       w_class('cms_actions').new(:tree_node => tree_node,
-                        :options => {:buttons => %W{ new_button },
-                          :resource_types => %W{ rss },
-                          :button_text => 'הוספת יחידות תוכן - עמודה שמאלית',
-                          :new_text => 'הוסף RSS נוסף',
-                          :has_url => false,
-                          :placeholder => 'left'}).render_to(self)
-              
-                      w_class('cms_actions').new(:tree_node => tree_node,
-                        :options => {:buttons => %W{ new_button },
-                          :resource_types => %W{ iframe },
-                          :button_text => 'הוספת יחידות iframe - עמודה שמאלית',
-                          :new_text => 'הוסף iframe נוסף',
-                          :has_url => false,
-                          :placeholder => 'left'}).render_to(self)
+                                                 :options => {:buttons => %W{ new_button },
+                                                              :resource_types => %W{ rss },
+                                                              :button_text => 'הוספת יחידות תוכן - עמודה שמ�?לית',
+                                                              :new_text => 'הוסף RSS נוסף',
+                                                              :has_url => false,
+                                                              :placeholder => 'left'}).render_to(self)
 
                       w_class('cms_actions').new(:tree_node => tree_node,
-                        :options => {:buttons => %W{ new_button },
-                          :resource_types => %W{ kabtv },
-                          :button_text => 'ניהול יחידת טלוויזיה',
-                          :new_text => 'הוספת יחידת טלוויזיה',
-                          :has_url => false,
-                          :placeholder => 'home_kabtv'}).render_to(self)
+                                                 :options => {:buttons => %W{ new_button },
+                                                              :resource_types => %W{ iframe },
+                                                              :button_text => 'הוספת יחידות iframe - עמודה שמ�?לית',
+                                                              :new_text => 'הוסף iframe נוסף',
+                                                              :has_url => false,
+                                                              :placeholder => 'left'}).render_to(self)
+
+                      w_class('cms_actions').new(:tree_node => tree_node,
+                                                 :options => {:buttons => %W{ new_button },
+                                                              :resource_types => %W{ kabtv },
+                                                              :button_text => 'ניהול יחידת טלוויזיה',
+                                                              :new_text => 'הוספת יחידת טלוויזיה',
+                                                              :has_url => false,
+                                                              :placeholder => 'home_kabtv'}).render_to(self)
 
                       #                      w_class('cms_actions').new(:tree_node => tree_node,
                       #                        :options => {:buttons => %W{ new_button },
@@ -164,16 +164,16 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
                       #                          :placeholder => 'left'}).render_to(self)
 
                       #show_content_resources(:resources => kabtv_resources,
-                        #:parent => :website,
-                        #:placeholder => :home_kabtv,
-                        #:sortable => false
+                      #:parent => :website,
+                      #:placeholder => :home_kabtv,
+                      #:sortable => false
                       #)
-                      
-                      div(:class => 'left-column'){
+
+                      div(:class => 'left-column') {
                         show_content_resources(:resources => left_column_resources,
-                          :parent => :website,
-                          :placeholder => :left,
-                          :sortable => true
+                                               :parent => :website,
+                                               :placeholder => :left,
+                                               :sortable => true
                         )
                       }
                       make_sortable(:selector => ".left-column") {
@@ -184,21 +184,21 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
                   div(:class => 'yui-u') {
                     div(:class => 'content') {
                       div(:class => 'h1') {
-                        text 'המומלצים'
-                        div(:class =>'h1-right')
-                        div(:class =>'h1-left')
+                        text 'המומלצי�?'
+                        div(:class => 'h1-right')
+                        div(:class => 'h1-left')
                       }
 
                       w_class('cms_actions').new(:tree_node => @tree_node,
-                        :options => {:buttons => %W{ new_button },
-                          :resource_types => %W{content_preview title},
-                          :new_text => 'הוסף תצוגה מקדימה',
-                          :button_text => 'הוספת יחידות תוכן - עמודה מרכזית',
-                          :has_url => false, :placeholder => 'middle'}).render_to(self)
+                                                 :options => {:buttons => %W{ new_button },
+                                                              :resource_types => %W{content_preview title},
+                                                              :new_text => 'הוסף תצוגה מקדימה',
+                                                              :button_text => 'הוספת יחידות תוכן - עמודה מרכזית',
+                                                              :has_url => false, :placeholder => 'middle'}).render_to(self)
                       show_content_resources(:resources => middle_column_resources,
-                        :parent => :website,
-                        :placeholder => :middle,
-                        :sortable => true)
+                                             :parent => :website,
+                                             :placeholder => :middle,
+                                             :sortable => true)
 
                       make_sortable(:selector => ".content") {
                         middle_column_resources
@@ -219,8 +219,8 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
               div(:class => 'right-part') {
                 div(:class => 'h1') {
                   text 'הקליפ היומי'
-                  div(:class =>'h1-right')
-                  div(:class =>'h1-left')
+                  div(:class => 'h1-right')
+                  div(:class => 'h1-left')
                 }
                 begin
                   video_gallery
@@ -229,8 +229,8 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
 
                   right_column
                 rescue Exception => e
-                  logger.error e.message
-                  logger.error e.backtrace.join('\n')
+                  puts e.message
+                  puts e.backtrace.join('\n')
                 end
               }
             }
@@ -245,89 +245,89 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
         @google_analytics.render_to(self)
       }
     }
-  end     
-  
-  private 
-  
+  end
+
+  private
+
   def right_column_resources
     @tree_nodes_right ||= TreeNode.get_subtree(
-      :parent => tree_node.id, 
-      :resource_type_hrids => ['site_updates', 'newsletter', 'banner'],
-      :depth => 1,
-      :placeholders => ['right'],
-      :status => ['PUBLISHED', 'DRAFT']
-    ) 
+        :parent => tree_node.id,
+        :resource_type_hrids => ['site_updates', 'newsletter', 'banner'],
+        :depth => 1,
+        :placeholders => ['right'],
+        :status => ['PUBLISHED', 'DRAFT']
+    )
   end
-  
+
   def right_column_video_gallery_resources
     @tree_nodes_right_video_gallery ||= TreeNode.get_subtree(
-      :parent => tree_node.id,
-      :resource_type_hrids => ['video_gallery'],
-      :depth => 1,
-      :placeholders => ['right'],
-      :status => ['PUBLISHED', 'DRAFT']
+        :parent => tree_node.id,
+        :resource_type_hrids => ['video_gallery'],
+        :depth => 1,
+        :placeholders => ['right'],
+        :status => ['PUBLISHED', 'DRAFT']
     )
   end
 
   def left_column_resources
     @tree_nodes_left ||= TreeNode.get_subtree(
-      :parent => tree_node.id, 
-      :resource_type_hrids => ['rss', 'iframe'],
-      :depth => 1,
-      :placeholders => ['left'],
-      :status => ['PUBLISHED', 'DRAFT']
-    ) 
+        :parent => tree_node.id,
+        :resource_type_hrids => ['rss', 'iframe'],
+        :depth => 1,
+        :placeholders => ['left'],
+        :status => ['PUBLISHED', 'DRAFT']
+    )
   end
-  
+
   def middle_column_resources
     @tree_nodes_middle ||= TreeNode.get_subtree(
-      :parent => tree_node.id, 
-      :resource_type_hrids => ['content_preview', 'title'], 
-      :depth => 1,
-      :placeholders => ['middle'],
-      :status => ['PUBLISHED', 'DRAFT']
-    ) 
+        :parent => tree_node.id,
+        :resource_type_hrids => ['content_preview', 'title'],
+        :depth => 1,
+        :placeholders => ['middle'],
+        :status => ['PUBLISHED', 'DRAFT']
+    )
   end
-  
+
   def kabbalah_media_resources
     @kabbalah_media_nodes ||= TreeNode.get_subtree(
-      :parent => tree_node.id, 
-      :resource_type_hrids => ['media_rss'], 
-      :depth => 1,
-      :placeholders => ['lesson'],
-      :status => ['PUBLISHED', 'DRAFT']
-    ) 
+        :parent => tree_node.id,
+        :resource_type_hrids => ['media_rss'],
+        :depth => 1,
+        :placeholders => ['lesson'],
+        :status => ['PUBLISHED', 'DRAFT']
+    )
   end
 
   def kabtv_resources
     @kabtv_nodes ||= TreeNode.get_subtree(
-      :parent => tree_node.id,
-      :resource_type_hrids => ['kabtv'],
-      :depth => 1,
-      :placeholders => ['home_kabtv'],
-      :status => ['PUBLISHED', 'DRAFT']
+        :parent => tree_node.id,
+        :resource_type_hrids => ['kabtv'],
+        :depth => 1,
+        :placeholders => ['home_kabtv'],
+        :status => ['PUBLISHED', 'DRAFT']
     )
   end
 
   def downloads
     return
-    div(:class => 'downloads container'){
+    div(:class => 'downloads container') {
       h3(:class => 'box_header') {
         text 'שיעור הקבלה היומי להורדה'
         w_class('cms_actions').new(:tree_node => tree_node,
-          :options => {:buttons => %W{ new_button },
-            :resource_types => %W{ media_rss },
-            :new_text => 'צור יחידת תוכן חדשה',
-            :mode => 'inline',
-            :button_text => 'הוספת הורדות',
-            :has_url => false,
-            :placeholder => 'lesson'}).render_to(self)
+                                   :options => {:buttons => %W{ new_button },
+                                                :resource_types => %W{ media_rss },
+                                                :new_text => 'צור יחידת תוכן חדשה',
+                                                :mode => 'inline',
+                                                :button_text => 'הוספת הורדות',
+                                                :has_url => false,
+                                                :placeholder => 'lesson'}).render_to(self)
       }
-      div(:class => 'entries'){
+      div(:class => 'entries') {
         show_content_resources(:resources => kabbalah_media_resources,
-          :parent => :website,
-          :placeholder => :left,
-          :sortable => true
+                               :parent => :website,
+                               :placeholder => :left,
+                               :sortable => true
         )
       }
       make_sortable(:selector => ".downloads .entries", :axis => 'y') {
@@ -338,43 +338,43 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
 
   def video_gallery
     w_class('cms_actions').new(:tree_node => tree_node,
-      :options => {:buttons => %W{ new_button },
-        :resource_types => %W{ video_gallery },
-        :new_text => 'צור יחידת תוכן חדשה',
-        :has_url => false,
-        :placeholder => 'right'}).render_to(self)
+                               :options => {:buttons => %W{ new_button },
+                                            :resource_types => %W{ video_gallery },
+                                            :new_text => 'צור יחידת תוכן חדשה',
+                                            :has_url => false,
+                                            :placeholder => 'right'}).render_to(self)
 
     show_content_resources(:resources => right_column_video_gallery_resources,
-      :parent => :website,
-      :placeholder => :right,
-      :sortable => true)  { |idx|
+                           :parent => :website,
+                           :placeholder => :right,
+                           :sortable => true) { |idx|
       @newsletter.render_to(self) if (idx == 1)
     }
   end
 
   def right_column
     w_class('cms_actions').new(:tree_node => tree_node,
-      :options => {:buttons => %W{ new_button },
-        :resource_types => %W{ site_updates newsletter banner},
-        :new_text => 'צור יחידת תוכן חדשה',
-        :has_url => false,
-        :placeholder => 'right'}).render_to(self)
+                               :options => {:buttons => %W{ new_button },
+                                            :resource_types => %W{ site_updates newsletter banner},
+                                            :new_text => 'צור יחידת תוכן חדשה',
+                                            :has_url => false,
+                                            :placeholder => 'right'}).render_to(self)
 
-		div(:class => 'right-part') {
-			show_content_resources(:resources => right_column_resources,
-				:parent => :website,
-				:placeholder => :right,
-				:sortable => true)  { |idx|
-        @newsletter.render_to(self) if (idx == 0)
-			}
+    div(:class => 'right-part') {
+      show_content_resources(:resources => right_column_resources,
+                             :parent => :website,
+                             :placeholder => :right,
+                             :sortable => true) { |idx|
+        @newsletter.render_to(self) if (idx == 0 && @newsletter)
+      }
     }
     make_sortable(:selector => ".right-part") {
       right_column_resources
     }
   end
 
-	def render_hardcoded_streamer(url, width, height)
-		rawtext <<-STREAM
+  def render_hardcoded_streamer(url, width, height)
+    rawtext <<-STREAM
 			<div id="stream" style="text-align: center; margin-bottom: 10px; background: #F0F4FD; padding: 5px;">
 			<div id="tvobj">
 			<!--[If IE]>
@@ -446,6 +446,6 @@ class Ligdoltv::Layouts::Website < WidgetManager::Layout
 			</div>
 			</div>
 
-		STREAM
+    STREAM
   end
 end 

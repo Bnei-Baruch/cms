0cdeb4348f7cfbc11519d0575f693785efbc0478
ramigg <ramigg@gmail.com>
2013-05-05 09:55:58 +0300

    Do not display name of block

--- app/sites/global/widgets/block_holder.rb
+++ app/sites/global/widgets/block_holder.rb
@@ -4,14 +4,12 @@ class Global::Widgets::BlockHolder < WidgetManager::Base
     debugger
     title = get_title
     block = Block.find_by_name get_block
-    name = get_block
 
     div(:class => 'box all-box-background') {
       div(:class => 'box-top'){rawtext('&nbsp;')}
       div(:class => 'box-mid'){
         
         h3 title unless title.empty?
-        h4 name unless name.empty?
         rawtext block.content unless block.nil?
         br
         w_class('cms_actions').new(:tree_node => tree_node, :options => {:buttons => %W{ delete_button  edit_button }}).render_to(self)

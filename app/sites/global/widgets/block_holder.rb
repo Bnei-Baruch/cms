9d0319e27487461e7ea1a2fb2fb4884685a51483
Gregory Shilin <gshilin@gmail.com>
2013-05-05 09:03:19 +0300

    blocks

---
+++ app/sites/global/widgets/block_holder.rb
@@ -0,0 +1,23 @@
+class Global::Widgets::BlockHolder < WidgetManager::Base
+  
+  def render_full
+    debugger
+    title = get_title
+    block = Block.find_by_name get_block
+    name = get_block
+
+    div(:class => 'box all-box-background') {
+      div(:class => 'box-top'){rawtext('&nbsp;')}
+      div(:class => 'box-mid'){
+        
+        h3 title unless title.empty?
+        h4 name unless name.empty?
+        rawtext block.content unless block.nil?
+        br
+        w_class('cms_actions').new(:tree_node => tree_node, :options => {:buttons => %W{ delete_button  edit_button }}).render_to(self)
+      }
+      div(:class => 'box-bot'){rawtext('&nbsp;')}
+    }
+  end
+  
+end

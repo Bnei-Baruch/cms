14cc7ab5105fd649d14988a40007c03c0684c077
Gregory Shilin <gshilin@gmail.com>
2013-05-30 17:23:10 +0300

    bugfix: empty title - show body

--- app/sites/mainsites/widgets/article.rb
+++ app/sites/mainsites/widgets/article.rb
@@ -2,20 +2,26 @@ class Mainsites::Widgets::Article < WidgetManager::Base
     
   def render_full
     w_class('cms_actions').new(:tree_node => tree_node, :options => {:buttons => %W{ delete_button  edit_button }, :position => 'bottom'}).render_to(self)
-    unless get_title.empty?
+
+    title = get_title
+    body = get_body
+
+    if title.empty?
+      rawtext body
+    else
       if get_hide_content
         h3(:class => 'hide_content') do
           a(:class => 'x-plus') {
             rawtext '&plus;'
           }
-          a get_title
+          a title
         end
         div(:class => 'hidden x-data')  do
-          rawtext get_body
+          rawtext body
         end
       else
-        h3 get_title
-        rawtext get_body
+        h3 title
+        rawtext body
       end
     end
 
